53674d7596703f8b16700661733ea2a634185cfe
UAV_CuteKitten <2436541069@qq.com>
2024-12-29 16:15:44 +0800

    sync

--- build/drone_controller/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/orangepi/Multitask-Drone/install/drone_interfaces/share/drone_interfaces/package.sh"
--- build/drone_interfaces/rosidl_generator_py/drone_interfaces/msg/__init__.py
+++
@@ -1,5 +0,0 @@
-from drone_interfaces.msg._panel_box import PanelBox  # noqa: F401
-from drone_interfaces.msg._raw_waypoint import RawWaypoint  # noqa: F401
-from drone_interfaces.msg._task import Task  # noqa: F401
-from drone_interfaces.msg._task_state import TaskState  # noqa: F401
-from drone_interfaces.msg._yaw import Yaw  # noqa: F401
