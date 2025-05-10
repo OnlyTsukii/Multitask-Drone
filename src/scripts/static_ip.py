14fd15673c4ec066c7109402151154c5955a56c2
CuteKitten <2436541069@qq.com>
2025-05-10 18:49:58 +0800

    fix bug

--- src/scripts/static_ip.py
+++ src/scripts/static_ip.py
@@ -1,10 +1,39 @@
+#!/bin/python
 import subprocess
-import psutil
+import logging
+import time
 
 SSID = "SDUA_WIFI"
 PASSWORD = "zhen1234..zz"
 IFNAME = "wlP1p1s0"
 IP_SUBFIX = "123"
+
+
+def scan_ssids():
+    try:
+        result = subprocess.run(
+            ["nmcli", "-t", "-f", "SSID", "dev", "wifi"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+        )
+        return result.stdout.decode().splitlines()
+    except subprocess.CalledProcessError as e:
+        logging.error("Wi-Fi scan failed: %s", e.stderr.decode())
+        return []
+
+
+logging.info("Waiting for SSID '%s'...", SSID)
+
+for _ in range(20):  # 最多等待20秒
+    ssids = scan_ssids()
+    if SSID in ssids:
+        logging.info("SSID '%s' found.", SSID)
+        break
+    time.sleep(1)
+else:
+    logging.error("SSID '%s' not found after timeout.", SSID)
+    exit(1)
 
 isconnected = False
 res = subprocess.run(
@@ -58,6 +87,7 @@ if nowIP.split(".")[-1] != IP_SUBFIX:
         check=True,
         stdout=subprocess.PIPE,
     )
-    subprocess.run(["nmcli", "connection", "up", IFNAME], check=True)
+    subprocess.run(["nmcli", "connection", "up", "SDUA_WIFI"], check=True)
 else:
     print("already set target ip")
+subprocess.run('echo "123456789" > test.txt', shell=True, check=True)
--- src/shell/start_hotspot.sh
+++ src/shell/start_hotspot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # set hotspot name and password
-SSID="uav"
-PASSWORD="12345678"
-IFNAME="wlP1p1s0"
+# SSID="uav"
+# PASSWORD="12345678"
+# IFNAME="wlP1p1s0"
 
-# start hotspot
-nmcli device wifi hotspot ifname $IFNAME ssid "$SSID" password "$PASSWORD"
+# # start hotspot
+# nmcli device wifi hotspot ifname $IFNAME ssid "$SSID" password "$PASSWORD"
