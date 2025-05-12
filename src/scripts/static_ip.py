474eb9aa8a76c5de0d7dc96b29b2224dca70f78d
CuteKitten <2436541069@qq.com>
2025-05-12 11:55:02 +0800

    add static ip list

--- src/scripts/static_ip.py
+++ src/scripts/static_ip.py
@@ -8,20 +8,22 @@ SSID = "uav123"
 PASSWORD = "123456789@"
 IFNAME = "wlo1"
 IP_SUBFIX = "122"
-
+STATIC_IPS = ["123","122"]
 KNOWN_MAC = ["dc:4a:9e:d3:46:29", "54:8d:5a:3b:86:ed"]
 
 
 def get_network_interfaces_with_mac():
     global IFNAME
+    global IP_SUBFIX
     interfaces = netifaces.interfaces()
 
     for iface in interfaces:
         addrs = netifaces.ifaddresses(iface)
         if netifaces.AF_LINK in addrs:
             mac = addrs[netifaces.AF_LINK][0].get("addr")
-            if mac == KNOWN_MAC[0] or mac == KNOWN_MAC[1]:
+            if mac in KNOWN_MAC:
                 IFNAME = iface
+                IP_SUBFIX = STATIC_IPS[KNOWN_MAC.index(mac)]
                 break
 
 
